fb62231c1febca0f456b15aa987572cb674d06e4
Eric Xu <eric@mac.home>
2025-09-12 16:22:48 -0400

    last step

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,15 +2,19 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int count = 0;
-        for (int i = 1; i < 1000; i ++) {
-            boolean div_by_3 = i % 3 == 0;
-            boolean div_by_5 = i % 5 == 0;
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int num = 0;
+        for (int i = 1; i < n; i ++) {
+            boolean div_by_3 = i % a == 0;
+            boolean div_by_5 = i % b == 0;
             if (div_by_3 || div_by_5) {
-                count ++;
+                num ++;
             }
         }
-        System.out.println(count);
-
+        return num;
     }
 }
